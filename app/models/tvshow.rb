df25cf8a8eb1d37ef52204a11e029b17c845f405
mcmarius <m.c.marius@outlook.com>
2017-04-11 23:49:01 +0300

    trying to create tv shows

--- app/models/tvshow.rb
+++ app/models/tvshow.rb
@@ -2,4 +2,29 @@ class Tvshow < ApplicationRecord
   has_many :episodes
   has_many :users, through: :followed_tvshows
   validates :imdb_id, uniqueness: true
+
+  KEYS = [:type]
+  def self.add_or_create(imdb_id)
+    require 'omdbapi'
+    unless Tvshow.all.find_by(imdb_id)
+      tvshow = OMDB.id(imdb_id)
+      common_create(tvshow)
+    end
+
+    @tvshow.save
+  end
+
+  def self.add_or_create_by_title(title)
+    require 'omdbapi'
+    unless Tvshow.all.find_by(title)
+      tvshow = OMDB.client.title(title)
+      common_create(tvshow)
+    end
+
+    @tvshow.save
+  end
+
+  def self.common_create(tvshow)
+    @tvshow = Tvshow.create(tvshow.except(*KEYS))
+  end
 end
