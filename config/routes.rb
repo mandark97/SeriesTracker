9978ccb780986ee8b285012a80670373234b2752
matei <melintetmatei@outlook.com>
2017-04-17 21:43:05 +0300

    user can follow Tvshow and mark episodes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'sessions#create'
 
   root to: 'tvshow_manager#index'
+  get 'tvshow_manager/show_tvshows'
+  get 'tvshow_manager/show_episodes'
+  get 'tvshow_manager/mark_episode'
   get '/show', to: 'tvshow_manager#show'
   get 'tvshow_manager/follow'
   delete '/logout', to: 'sessions#destroy'
--- db/migrate/20170408121523_create_episodes.rb
+++ db/migrate/20170408121523_create_episodes.rb
@@ -1,6 +1,6 @@
 class CreateEpisodes < ActiveRecord::Migration[5.0]
   def change
-    create_table :episodes do |t|
+    create_table :@episodes do |t|
       t.belongs_to :tvshow, index: true, foreing_key: true
       t.string :imdb_id, index: true,unique: true
       t.string :title
