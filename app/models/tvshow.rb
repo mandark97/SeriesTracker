992e42d0161f28888dd23878622e53561127c8e7
mcmarius <m.c.marius@outlook.com>
2017-04-18 22:00:44 +0300

    Peasant merge (create tv show)

--- app/models/tvshow.rb
+++ app/models/tvshow.rb
@@ -1,28 +1,44 @@
+# documentation
 class Tvshow < ApplicationRecord
   has_many :episodes
   has_many :followed_tvshows
   has_many :users, through: :followed_tvshows
   validates :imdb_id, uniqueness: true
 
-  KEYS = [:type, :language, :answer, :country, :awards, :metascore, :response]
   def self.add_or_create(imdb_id)
-    unless (ret=Tvshow.all.find_by(imdb_id: imdb_id))
-      tvshow = OMDB.id(imdb_id)
-      ret=common_create(tvshow)
+    unless (ret = Tvshow.all.find_by(imdb_id: imdb_id))
+      tvshow = OMDB.client.id(imdb_id)
+      ret = common_create(tvshow)
     end
     ret
   end
 
   def self.add_or_create_by_title(title)
-    unless (ret=Tvshow.all.find_by(title: title))
+    unless (ret = Tvshow.all.find_by(title: title))
       tvshow = OMDB.client.title(title)
-      ret=common_create(tvshow)
+      ret = common_create(tvshow)
     end
     ret
-
   end
 
   def self.common_create(tvshow)
-    return Tvshow.create(tvshow.except(*KEYS))
+    show = Tvshow.new
+    show.title = tvshow[:title]
+    show.imdb_id = tvshow[:imdb_id]
+    show.year = tvshow[:year]
+    show.rated = tvshow[:rated]
+    show.released = tvshow[:released]
+    show.runtime = tvshow[:runtime]
+    show.genre = tvshow[:genre]
+    show.director = tvshow[:director]
+    show.writer = tvshow[:writer]
+    show.actors = tvshow[:actors]
+    show.plot = tvshow[:plot]
+    show.poster = tvshow[:poster]
+    show.imdb_rating = tvshow[:imdb_rating]
+    show.imdb_votes = tvshow[:imdb_votes]
+    show.total_seasons = tvshow[:total_seasons]
+    show.save
+    show
   end
 end
