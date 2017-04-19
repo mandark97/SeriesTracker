78112dd3e7e14dd988d20706544cf8336e63db5c
Mihailescu Cristina <mihailescucristina@outlook.com>
2017-04-19 22:33:57 +0200

    post on twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,13 @@ class User < ApplicationRecord
     )
     user
   end
+
+  def twitter
+    @client ||= Twitter::REST::Client.new do |config|
+      config.consumer_key        = Rails.application.secrets.twitter_api_key
+      config.consumer_secret     = Rails.application.secrets.twitter_api_secret
+      config.access_token        = token
+      config.access_token_secret = secret
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'tweets/index'
+
+  get 'tweets/show'
+
+  get 'tweets/new'
+
+  get 'tweets/edit'
+
   get '/auth/:provider/callback', to: 'sessions#create'
 
   root to: 'tvshow_manager#index'
