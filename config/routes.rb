02843b2aa96f45eec218cdafb5b992228720493f
matei <melintetmatei@outlook.com>
2017-04-24 10:17:12 +0300

    unfollow tvshows just backend

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   root to: 'tvshow_manager#index'
   get 'tvshow_manager/search', as: 'search'
   get 'tvshow_manager/follow/:imdb_id', to: 'tvshow_manager#follow', as: 'follow_tvshow'
+  get 'tvshow_manager/unfollow/:id', to: 'tvshow_manager#unfollow'
   get 'tvshow_manager/watchlist', as: 'watchlist'
   get 'tvshow_manager/tvshow/:id', to: 'tvshow_manager#tvshow_details', as: 'tvshow_details'
 
