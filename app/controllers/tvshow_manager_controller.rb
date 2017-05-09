c1da45398762d8436258c222d230461ce7f0a66b
Oana Plesu <plesu.oana@gmail.com>
2017-05-10 02:09:32 +0300

    watchlist view modified

--- app/controllers/tvshow_manager_controller.rb
+++ app/controllers/tvshow_manager_controller.rb
@@ -62,16 +62,19 @@ class TvshowManagerController < ApplicationController
                 message_type: 'alert-success'
   end
 
-  # return a view displaying a user's watchlist
-  def watchlist
-    @tvshows_list = current_user.tvshows
+  def unfollow
+    tvshow = current_user.followed_tvshows.find_by(tvshow_id:params[:id])
+    tvshow.followed_episodes.each do |episode|
+      episode.destroy
+    end
+    tvshow.destroy
+    redirect_to watchlist_path
   end
 
   # return a view containing all the details for a tv show
   # and a list for each season with it's coresponding episodes
   def tvshow_details
-    @f_tvshow = FollowedTvshow.find_by_tvshow_id(params[:id])
-
+    @f_tvshow = FollowedTvshow.find_by(tvshow_id: params[:id])
     if params[:message_text]
       @message = {
           'text': params[:message_text],
@@ -99,5 +102,32 @@ class TvshowManagerController < ApplicationController
     end
 
     @episodes_this_week = episodes_of_the_week
+  end
+
+  def watchlist
+    @watchlist = {
+        'new_this_week': [],
+        'last_episodes': [],
+        'begin_watching': []
+    }
+
+    beginning_of_week = Time.current.utc.beginning_of_week
+    end_of_week = Time.current.utc.end_of_week
+
+    current_user.followed_tvshows.each do |f_tvshow|
+      episode = f_tvshow.tvshow.episodes.find_by(released: beginning_of_week..end_of_week)
+
+      if !episode.nil? #tvshow has new episode this week
+        @watchlist.new_this_week << episode
+      else #show last watched episode
+        f_episode = f_tvshow.followed_episodes.order(:created_at).last
+
+        if f_episode.nil? # the user hasn't begin to watch the tvshow yet
+          @watchlist.begin_watching << f_tvshow.tvshow
+        else
+          @watchlist.last_episodes << f_episode.episode
+        end
+      end
+    end
   end
 end
