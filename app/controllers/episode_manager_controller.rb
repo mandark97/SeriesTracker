6a49fa812426eca20d73b3699c130b73e053ce14
matei <melintetmatei@outlook.com>
2017-05-14 23:45:39 +0300

    merge to master

--- app/controllers/episode_manager_controller.rb
+++ app/controllers/episode_manager_controller.rb
@@ -35,28 +35,6 @@ class EpisodeManagerController < ApplicationController
         episodes << ep
       end
     end
-    redirect_to tvshow_details_path id: params[:show_id], anchor: "season#{ params[:season_nr] }"
-  end
-
-
-  # toggle all episodes from a season
-  def toggle_all
-    f_tvshow = current_user.followed_tvshows.find_by(tvshow_id: params[:show_id])
-    episodes = f_tvshow.episodes
-    del = false
-    if Episode.where(tvshow_id: f_tvshow.tvshow.id, season: params[:season_nr]).count == f_tvshow.episodes.where(season: params[:season_nr]).count
-      del = true
-    end
-    Episode.where(tvshow_id: f_tvshow.tvshow_id, season: params[:season_nr]).each do |ep|
-      e = f_tvshow.followed_episodes.find_by(episode_id: ep.id)
-      if del
-        unless e.nil?
-          e.delete
-        end
-      elsif !episodes.include?(ep)
-        episodes << ep
-      end
-    end
     redirect_to tvshow_details_path id: params[:show_id]
   end
 
