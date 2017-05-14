0b97736345c64c237f56e1dad910e8a0e7339e5b
matei <melintetmatei@outlook.com>
2017-05-14 15:51:14 +0300

    toggle all

--- app/controllers/episode_manager_controller.rb
+++ app/controllers/episode_manager_controller.rb
@@ -14,7 +14,28 @@ class EpisodeManagerController < ApplicationController
                                       message_type: 'alert-danger'
       return
     end
-    redirect_to tvshow_details_path id: episode.tvshow_id, anchor: "season#{ episode.season }"
+    redirect_to tvshow_details_path id: episode.tvshow_id, :anchor => "season#{ episode.season }"
+  end
+
+  # toggle all episodes from a season
+  def toggle_all
+    f_tvshow = current_user.followed_tvshows.find_by(tvshow_id: params[:show_id])
+    episodes = f_tvshow.episodes
+    del = false
+    if Episode.where(tvshow_id: f_tvshow.tvshow.id, season: params[:season_nr]).count == f_tvshow.episodes.where(season: params[:season_nr]).count
+      del = true
+    end
+    Episode.where(tvshow_id: f_tvshow.tvshow_id, season: params[:season_nr]).each do |ep|
+      e = f_tvshow.followed_episodes.find_by(episode_id: ep.id)
+      if del
+        unless e.nil?
+          e.delete
+        end
+      elsif !episodes.include?(ep)
+        episodes << ep
+      end
+    end
+    redirect_to tvshow_details_path id: params[:show_id]
   end
 
   # marks an episode as unwatched and redirects them
@@ -33,10 +54,15 @@ class EpisodeManagerController < ApplicationController
       return
     end
 
-    redirect_to tvshow_details_path id: followed_tvshow.id, anchor: "season#{ episode.season }"
+    redirect_to tvshow_details_path id: followed_tvshow.id, :anchor => "season#{ episode.season }"
   end
 
   def episode_details
     @episode = Episode.find(params[:id])
+  end
+
+  private
+  def check_login
+    redirect_to root_path unless current_user
   end
 end
