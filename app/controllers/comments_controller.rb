4c96ce589c0ac897717c3870c344cbd6fa47b1ca
Mihailescu Cristina <mihailescucristina@outlook.com>
2017-05-14 17:02:23 +0200

    add comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+  before_action :check_login
+
+  def create
+    @comment = @commentable.comments.new comment_params
+    @comment.user = current_user
+    @comment.save
+    redirect_to(:back)
+  end
+
+  private
+
+    def check_login
+      redirect_to root_path unless current_user
+    end
+
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
+end
