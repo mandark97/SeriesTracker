f164669af9f610ef990bda6a8a763e7edad48b16
Melinte Matei <melintetmatei@outlook.com>
2017-05-16 00:17:14 +0300

    Tests (#34)
    * more tests some refactoring

--- app/controllers/episode_manager_controller.rb
+++ app/controllers/episode_manager_controller.rb
@@ -17,6 +17,25 @@ class EpisodeManagerController < ApplicationController
     redirect_to tvshow_details_path id: episode.tvshow_id, anchor: "season#{ episode.season }"
   end
 
+  # marks an episode as unwatched and redirects them
+  # to the tvshow_details view
+  def unfollow
+    episode = Episode.find(params[:id])
+    followed_tvshow = current_user.followed_tvshows.find_by(tvshow_id: episode.tvshow_id)
+    followed_episode = followed_tvshow.followed_episodes.find_by(episode_id: params[:id])
+
+    begin
+      followed_episode.delete
+    rescue
+      redirect_to tvshow_details_path id: episode.tvshow_id,
+                                      message_text: 'An error occurred while marking the episode as unwatched',
+                                      message_type: 'alert-danger'
+      return
+    end
+
+    redirect_to tvshow_details_path id: episode.tvshow_id, anchor: "season#{ episode.season }"
+  end
+
   # toggle all episodes from a season
   def toggle_all
     f_tvshow = current_user.followed_tvshows.find_by(tvshow_id: params[:show_id])
@@ -36,25 +55,6 @@ class EpisodeManagerController < ApplicationController
       end
     end
     redirect_to tvshow_details_path id: params[:show_id]
-  end
-
-  # marks an episode as unwatched and redirects them
-  # to the tvshow_details view
-  def unfollow
-    episode = Episode.find(params[:id])
-    followed_tvshow = current_user.followed_tvshows.find_by(tvshow_id: episode.tvshow_id)
-    followed_episode = followed_tvshow.followed_episodes.find_by(episode_id: params[:id])
-
-    begin
-      followed_episode.delete
-    rescue
-      redirect_to tvshow_details_path id: episode.tvshow_id,
-                                      message_text: 'An error occurred while marking the episode as unwatched',
-                                      message_type: 'alert-danger'
-      return
-    end
-
-    redirect_to tvshow_details_path id: episode.tvshow_id, anchor: "season#{ episode.season }"
   end
 
   def episode_details
