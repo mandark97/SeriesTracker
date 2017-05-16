4425653baebc1b4a6c187f509449df5eb4ce3fc8
Oana Plesu <plesu.oana@gmail.com>
2017-05-17 00:23:46 +0300

    showing succes/error messages refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,10 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
   helper_method :current_user
   def check_login
-    redirect_to root_path unless current_user
+    redirect_to root_path 'message_type': 'alert-warning',
+                          'message_text': 'You need to be logged in to access this feature' unless current_user
   end
 end
